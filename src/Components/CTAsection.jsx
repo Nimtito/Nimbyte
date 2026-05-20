7b9d3f277d60b842437deed534589c1284848f6f
nimrod <nimrodtitus12@gmail.com>
2026-05-20 17:02:58 +0300

    Add CTAsection.jsx file and its content

---
+++ src/Components/CTAsection.jsx
@@ -0,0 +1,19 @@
+function CTASection() {
+  return (
+    <section className="bg-blue-600 text-white text-center py-20 px-6">
+      <h1 className="text-5xl font-bold">
+        Let's Build Your Next Digital Product
+      </h1>
+
+      <p className="mt-6 text-lg">
+        Start your journey with Nimbyte today.
+      </p>
+
+      <button className="mt-8 bg-white text-blue-600 px-6 py-3 rounded-xl font-semibold">
+        Request Service
+      </button>
+    </section>
+  )
+}
+
+export default CTASection
