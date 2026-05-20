7978a3091dec0236569cb80409b6e053590af97e
nimrod <nimrodtitus12@gmail.com>
2026-05-20 16:51:56 +0300

    Add Services.jsx file and its content

---
+++ src/Components/Services.jsx
@@ -0,0 +1,25 @@
+import services from "../../data/services"
+import ServiceCard from "../ServiceCard/ServiceCard"
+
+function Services() {
+  return (
+    <section className="px-8 py-20 bg-gray-50">
+      <h1 className="text-4xl font-bold text-center">
+        Our Services
+      </h1>
+
+      <div className="grid md:grid-cols-3 gap-8 mt-14 max-w-6xl mx-auto">
+        {services.map((service) => (
+          <ServiceCard
+            key={service.id}
+            title={service.title}
+            description={service.description}
+            image={service.image}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Services
